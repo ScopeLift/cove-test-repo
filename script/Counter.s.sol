188587df6652484e64590127f6ae3038c0aa93e3
Matt Solomon <matt@mattsolomon.dev>
2023-03-16 14:50:50 -0700

    chore: deploy to goerli

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {Counter} from "src/Counter.sol";
 
-contract CounterScript is Script {
-  function setUp() public {}
-
+contract DeployCounter is Script {
   function run() public {
     vm.broadcast();
+    new Counter();
   }
 }
